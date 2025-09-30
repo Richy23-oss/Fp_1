ddca48ebe3a84509607609b5aab653184368a03a
Richy23-oss <riardo.bencomz@gmail.com>
2025-09-30 16:58:29 -0600

    Add files via upload

---
+++ 25550470_Exa1/src/pkg25550470_exa1/Main.java
@@ -0,0 +1,33 @@
+
+package pkg25550470_exa1;
+
+import java.util.Scanner;
+
+
+public class Main {
+final 
+    
+    public static void main(String[] args) {
+     int saldo_inicial = 5000, pin =1234;
+     String usuario, contraseña;
+     Scanner input = new Scanner(System.in);
+     System.out.println("Bienvenido a tu cajero favorito:");
+     System.out.println("ingresa tu usuario:");
+     usuario = input.nextLine();
+     System.out.println("ingresa tu pin:");
+     contraseña = input.nextLine();
+     if (usuario.equals(usuario)){
+         if (contraseña.equals(contraseña)){
+             System.out.println("Bienvenido a nuestro sistema, selecciona lo que buscas");
+             System.out.println("Consultar tu saldo");
+             System.out.println("retirar dinero");
+             System.out.println("Depositar dinero");
+             
+         }
+     }
+     
+     
+     
+    }
+    
+}
